3dd1679a393ddde487cca729553b2eec97322677
NickShhh <nickaa25chitty@gmail.com>
2025-07-04 22:12:40 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,15 @@
-const mysql = require("mysql2/promise");
+// db.js
+const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: "shinkansen.proxy.rlwy.net",
+  host: 'shinkansen.proxy.rlwy.net',
+  user: 'root',
+  password: 'oquhiVlMRbvIjHFwyklvMSUaBaMdxKrq',
+  database: 'railway',
   port: 23303,
-  user: "root",
-  password: "oquhiVlMRbvIjHFwyklvMSUaBaMdxKrq",
-  database: "railway",
   waitForConnections: true,
   connectionLimit: 10,
+  queueLimit: 0
 });
 
 module.exports = pool;
